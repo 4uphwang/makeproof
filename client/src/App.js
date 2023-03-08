4701ccb6a9da8e78e67f88bc85b1342b5e516dbc
4uphwang <4uphwang@gmail.com>
2023-03-08 20:29:24 +0900

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,21 @@
+
+// import { useState } from 'react';
+import './App.css';
+// import download from "downloadjs";
+import MakeProof from './MakeProof';
+// import { Buffer } from "buffer";
+// const snarkjs = require("snarkjs");
+
+
+function App() {
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <MakeProof/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/config.js
@@ -0,0 +1,6 @@
+
+import axios from "axios"
+
+export const axiosInstance = axios.create({
+    baseURL: "https://yourd-makeproof.herokuapp.com/"
+})
